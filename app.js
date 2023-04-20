e86d8a5f77a5d23446eed31d079ec42f57c6d74c
unknown <dnioko0412@gmail.com>
2023-04-20 16:03:02 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// var a ;
+// var b;
+// var c;
+// var v;
+// var g;
+// g = "sekki"
+// c = "-"
+// a = "Umbujin";
+// b = "Teneg";
+// v = "Namhan"
+// console.log(a + c + b + c + v + c + g   );
+
+var a;
+var b;
+var c;
+
+prompt
+
+a = 15;
+
+
